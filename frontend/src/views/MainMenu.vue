bd9be2bfce3efcc71b3f8efaeb6558d297a020f9
Aron Hidalgo <aron.hidalgo@ucr.ac.cr>
2025-04-25 20:14:34 -0600

    feat(view-main-menu): add initial implementation for employer, software manager and payroll manager options

--- frontend/src/views/MainMenu.vue
+++ frontend/src/views/MainMenu.vue
@@ -51,11 +51,91 @@
         </div>
       </div>
 
-       <!-- Request-Hours-Correction card -->
-       <div v-if="showOption('viewRequestHoursCorrection')" class="col-md-3">
+      <!-- Request-Hour-Correction card -->
+      <div v-if="showOption('viewRequestHourCorrection')" class="col-md-3">
         <div class="card text-center h-100 shadow-sm">
           <div class="card-body d-flex flex-column justify-content-between">
             <h5 class="card-title">Solicitar Corrección en Horas</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Company card -->
+      <div v-if="showOption('viewCompany')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Empresa</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Employees card -->
+      <div v-if="showOption('viewEmployees')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Empleados</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Reports and payments card -->
+      <div v-if="showOption('viewReportsPayments')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Reportes y Pagos</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Role assignment card -->
+      <div v-if="showOption('viewRoleAssignment')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Asignación de Roles</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Company Management card -->
+      <div v-if="showOption('viewCompanyManagement')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Gestión de empresas</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Reports card -->
+      <div v-if="showOption('viewReports')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Reportes</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Generate payroll card -->
+      <div v-if="showOption('viewGeneratePayroll')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Generar Planillas</h5>
+            <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Payroll History card -->
+      <div v-if="showOption('viewPayrollHistory')" class="col-md-3">
+        <div class="card text-center h-100 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">Historial de Planillas</h5>
             <button class="btn btn-dark mt-3" mt-auto>Ir al sitio</button>
           </div>
         </div>
@@ -69,21 +149,51 @@ export default {
   name: "MainMenu",
   data() {
     return {
-      employeeType: "employee", // change different roles
+      employeeType: "payrollManager", // change different roles
     };
   },
   methods: {
     showOption(option) {
       const permissions = {
-        employee: ["viewProfile", "viewPayments", "viewWorkedHours", "viewBenefits"],
+        employee: [
+          "viewProfile", 
+          "viewPayments", 
+          "viewWorkedHours", 
+          "viewBenefits"],
+
+        employer: [
+          "viewProfile",
+          "viewCompany",
+          "viewEmployees",
+          "viewBenefits",
+          "viewReportsPayments",
+          "viewRoleAssignment",
+        ],
+
         supervisor: [
           "viewProfile",
           "viewPayments",
           "viewWorkedHours",
           "viewBenefits",
           "viewHours",
-          "viewRequestHoursCorrection",
+          "viewRequestHourCorrection",
         ],
+
+        softwareManager: [
+          "viewProfile",
+          "viewCompanyManagement",
+          "viewReports",
+        ],
+
+        payrollManager: [
+          "viewProfile",
+          "viewPayments",
+          "viewWorkedHours",
+          "viewBenefits",
+          "viewGeneratePayroll",
+          "viewPayrollHistory",
+        ],
+
       };
 
       return permissions[this.employeeType].includes(option);
@@ -102,5 +212,7 @@ export default {
 }
 .card:hover {
   transform: scale(1.05);
+  background-color: rgb(221, 221, 221);
+  border-color: black;
 }
 </style>
