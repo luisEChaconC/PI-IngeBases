4b03b57d106c1916518eee0b45f1b656231549c3
Nathalia Rodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-04-26 16:46:31 -0600

    chore: create the base of the api backend

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetUserByEmail")]
+        public IActionResult GetUserByEmail(string email)
+        {
+            return Ok(new { message = "Hello from UserController!" });
+        }
+
+    }
+}
---
+++ backend/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public UserModel(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
