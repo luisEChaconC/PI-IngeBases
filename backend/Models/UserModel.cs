695055377cfcdf503ca6d7cd6ee5481c9d13e6f5
Nathalia Rodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-05-01 18:32:19 -0600

    feat: create controller, model, and repository to check if an user exists

---
+++ backend/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿namespace backend.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public UserModel()
+        {
+            Id = 0;
+            Email = string.Empty;
+            Password = string.Empty;
+            IsAdmin = false;
+        }
+
+        public UserModel(int id, string email, string password, bool isAdmin)
+        {
+            Id = id;
+            Email = email;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+    }
+}
