7dc7fb5bed17738f6121b699614954404071b224
dracor03 <rvegassanabria@gmail.com>
2025-05-03 22:52:34 -0600

    feat: <frontend benefitView> BenefitView component has been done

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import BenefitList from '@/components/BenefitList.vue' 
+import Benefit from '@/components/BenefitView.vue'
 
-const routes = []
+const routes =
+[
+    {
+        path: '/benefits',
+        name: 'Benefits',
+        component: BenefitList
+    },
+
+    {
+        path: '/benefit/:id',
+        name: 'Benefit',
+        component: Benefit,
+        props: true
+    }
+
+]
 
 const router = createRouter({
     history: createWebHistory(),
