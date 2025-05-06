2eec6f84e7afd39cefc61d29c52246fec4d9c8f7
Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-05-06 21:28:28 +0000

    fix: define missing alias for src directory
    - Defined the @ alias in frontend/vue.config.js to point to the src directory.
    - Updated the import statements in frontend/src/router/index.js to use the @ alias instead of relative paths.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CompanyRegistration from '@/views/CompanyRegistration.vue'
-import EmployeesList from '../views/EmployeesList.vue'
-import Login from '../views/LoginForm.vue'
-import BenefitList from '@/components/BenefitList.vue' 
+import EmployeesList from '@/views/EmployeesList.vue'
+import Login from '@/views/LoginForm.vue'
+import BenefitList from '@/components/BenefitList.vue'
 import Benefit from '@/components/BenefitView.vue'
 import BenefitCreate from '@/components/BenefitCreate.vue'
 import SelectChangeBenefit from '@/components/SelectChangeBenefit.vue'
 
 const routes =
-[
-    { path: '/login', name: 'LoginForm', component: Login },
-    { path: '/company-registration', name: 'CompanyRegistration', component: CompanyRegistration},
-    { path: '/employees-list', name: 'EmployeesList', component: EmployeesList },
-    { path: '/benefits', name: 'Benefits', component: BenefitList },
-    { path: '/benefit/:id', name: 'Benefit', component: Benefit, props: true },
-    { path: '/benefit/create', name: 'CreateBenefit', component: BenefitCreate },
-    { path: '/select-change-benefit', name: 'SelectChangeBenefit', component: SelectChangeBenefit },
-]
+    [
+        { path: '/login', name: 'LoginForm', component: Login },
+        { path: '/company-registration', name: 'CompanyRegistration', component: CompanyRegistration },
+        { path: '/employees-list', name: 'EmployeesList', component: EmployeesList },
+        { path: '/benefits', name: 'Benefits', component: BenefitList },
+        { path: '/benefit/:id', name: 'Benefit', component: Benefit, props: true },
+        { path: '/benefit/create', name: 'CreateBenefit', component: BenefitCreate },
+        { path: '/select-change-benefit', name: 'SelectChangeBenefit', component: SelectChangeBenefit },
+    ]
 
 const router = createRouter({
     history: createWebHistory(),
