eb6b86392e2ab45d18a169f879afbdfc20f6f27c
andyhr12004 <andy.hernandezrojas@ucr.ac.cr>
2025-05-05 19:53:25 -0600

    feat: backend of Get Employee by ID, Model, Repository and Controller

---
+++ backend/Models/EmployeeGetIDModel.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace backend.Models
+{
+    public class EmployeeGetIDModel
+    {
+        public string Id { get; set; }
+        public string WorkerId { get; set; }
+        public string CompanyId { get; set; }
+        public DateTime EmployeeStartDate { get; set; }
+        public string ContractType { get; set; }
+        public decimal GrossSalary { get; set; }
+        public bool HasToReportHours { get; set; }
+
+        public string FirstName { get; set; }
+        public string FirstSurname { get; set; }
+        public string SecondSurname { get; set; }
+        public string Cedula { get; set; }
+        public string Email { get; set; }
+        public bool? IsAdmin { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
