93882359d7da0956935e5b2ab30901a150bd2773
Aron Hidalgo <aron.hidalgo@ucr.ac.cr>
2025-05-20 21:38:17 -0600

    fix(fixed-navigation-menu): add main layout, for initial implementation

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MainMenu />
+    <div class="content-area">
+      <router-view />  
+    </div>
+  </div>
+</template>
+
+<script>
+import MainMenu from '@/views/MainMenu.vue';
+
+export default {
+  components: { MainMenu },
+};
+</script>
+
+<style>
+.content-area {
+  padding: 20px;
+ 
+}
+</style>
