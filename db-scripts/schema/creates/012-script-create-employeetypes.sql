67cc399d0d817da39f55cd5baf9fc6e9ffb43714
dracor03 <rvegassanabria@gmail.com>
2025-05-26 10:39:01 -0600

    fix: <SelectChangeBenefit> Filter benefit per contract

--- db-scripts/schema/creates/012-script-create-employeetypes.sql
+++ db-scripts/schema/creates/012-script-create-employeetypes.sql
@@ -5,5 +5,9 @@ CREATE TABLE EmployeeTypes (
        NOT NULL 
        PRIMARY KEY 
        DEFAULT NEWID(),
-    Name VARCHAR(30) CHECK (Name IN ('Full-Time', 'Part-Time', 'Professional Services', 'Hourly')) NOT NULL, 
+    Name VARCHAR(30) CHECK (Name IN ('Full-Time', 'Part-Time', 'Professional Services')) NOT NULL
 );
+
+INSERT INTO EmployeeTypes (Name) VALUES ('Full-Time')
+INSERT INTO EmployeeTypes (Name) VALUES ('Part-Time')
+INSERT INTO EmployeeTypes (Name) VALUES ('Professional Services')
