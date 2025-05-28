b4287d0dc0259257ea96017450212982a46255a8
nathRodriguez <89035907+nathRodriguez@users.noreply.github.com>
2025-05-27 19:22:55 -0600

    chore : apply clean architecture

---
+++ backend/Domain/EmployerGetIDModel.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace backend.Domain
+{
+    public class EmployerGetIDModel
+    {
+        public string Id { get; set; }
+        public string CompanyId { get; set; }
+        public string WorkerId { get; set; }
+        public string FirstName { get; set; }
+        public string FirstSurname { get; set; }
+        public string SecondSurname { get; set; }
+        public string Cedula { get; set; }
+        public string Email { get; set; }
+        public bool? IsAdmin { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ backend/Domain/PayrollManagerModel.cs
@@ -0,0 +1,17 @@
+﻿namespace backend.Domain
+{
+    public class PayrollManagerModel
+    {
+        public string Id { get; set; }
+
+        public PayrollManagerModel()
+        {
+            Id = string.Empty;
+        }
+
+        public PayrollManagerModel(string id)
+        {
+            Id = id;
+        }
+    }
+}
