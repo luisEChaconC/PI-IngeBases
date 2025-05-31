87f49ddb58930098c4218827f5acc5af8fa64559
andyhr12004 <andy.hernandezrojas@ucr.ac.cr>
2025-05-30 20:51:05 -0600

    feat(income-tax-deduction): added logic of IncomeTax in IncomeTaxDeductionStrategy and calculates TotalTax

--- backend/Domain/Strategies/IncomeTaxDeductionStrategy.cs
+++ backend/Domain/Strategies/IncomeTaxDeductionStrategy.cs
@@ -4,7 +4,33 @@ namespace backend.Domain.Strategies
     {
         public decimal CalculateDeduction(decimal grossSalary, Benefit? benefit = null)
         {
-            return 2000;
+            decimal totalTax = 0;
+
+            if (grossSalary > 4_745_000m)
+            {
+                totalTax += (grossSalary - 4_745_000m) * 0.25m;
+                grossSalary = 4_745_000m;
+            }
+
+            if (grossSalary > 2_373_000m)
+            {
+                totalTax += (grossSalary - 2_373_000m) * 0.20m;
+                grossSalary = 2_373_000m;
+            }
+
+            if (grossSalary > 1_352_000m)
+            {
+                totalTax += (grossSalary - 1_352_000m) * 0.15m;
+                grossSalary = 1_352_000m;
+            }
+
+            if (grossSalary > 922_000m)
+            {
+                totalTax += (grossSalary - 922_000m) * 0.10m;
+                
+            }
+
+            return Math.Round(totalTax, 2);
         }
     }
 }
