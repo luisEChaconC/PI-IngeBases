467966c862ad14cfcc0b5a4b6dd3539008424cd7
dracor03 <rvegassanabria@gmail.com>
2025-06-01 18:06:15 -0600

    chore: <natural-person-gender> add comment to avoid compilation error

--- db-scripts/schema/02X-script-alter-natural-person-gender.sql
+++ db-scripts/schema/02X-script-alter-natural-person-gender.sql
@@ -4,6 +4,8 @@ ALTER TABLE NaturalPersons
 ADD Gender CHAR(1)
     CONSTRAINT CHK_Employees_Gender CHECK (Gender IN ('F', 'M'));
 
+-- Ejecutar primero sentencia de arriba y luego la de abajo
+
 UPDATE NaturalPersons
 SET Gender = 'M'
 WHERE Gender IS NULL;
