bcea7b078e69c6c7d5d6b3f69b30dc5c6197511f
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-06-04 13:11:20 -0600

    feat: register new services for pending approvals and day approval commands

--- backend/Program.cs
+++ backend/Program.cs
@@ -48,6 +48,9 @@ builder.Services.AddScoped<IGetEmployeeHoursInPeriodQuery, GetEmployeeHoursInPer
 builder.Services.AddScoped<IGetEmployeeTimesheetByDateQuery, GetEmployeeTimesheetByDateQuery>();
 builder.Services.AddScoped<IUpdateDayCommand, UpdateDayCommand>();
 builder.Services.AddScoped<IUpdatePayrollIdInTimesheetsCommand, UpdatePayrollIdInTimesheetsCommand>();
+builder.Services.AddScoped<IGetPendingApprovalsByEmployeeQuery, GetPendingApprovalsByEmployeeQuery>();
+builder.Services.AddScoped<IGetPendingDaysByEmployeeQuery, GetPendingDaysByEmployeeQuery>();
+builder.Services.AddScoped<IApproveDayCommand, ApproveDayCommand>();
 
 builder.Services.AddScoped<IPayrollRepository, PayrollRepository>();
 builder.Services.AddScoped<IGetPayrollsByCompanyIdQuery, GetPayrollsByCompanyIdQuery>();
