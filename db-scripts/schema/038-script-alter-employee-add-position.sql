dab9815c1d78d80c93396ebc219efca64f3a3516
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-06-26 12:31:50 -0600

    feat(employee-schema): update Position column to be NOT NULL with default value

--- db-scripts/schema/038-script-alter-employee-add-position.sql
+++ db-scripts/schema/038-script-alter-employee-add-position.sql
@@ -1,9 +1,2 @@
 ALTER TABLE Employees
-ADD Position VARCHAR(150)
-
-UPDATE Employees
-SET Position = 'Ingeniero de Software'
-WHERE Position IS NULL
-
-ALTER TABLE Employees
-ALTER COLUMN Position VARCHAR(150) NOT NULL
+ADD Position VARCHAR(150) NOT NULL DEFAULT 'Sin especialización'
