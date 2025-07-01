7305c50e3a2559524bffee4ea85c6e25b0b8bf0d
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-07-01 11:37:39 -0600

    feat: add IEmailService interface

---
+++ backend/Application/IEmailService.cs
@@ -0,0 +1,10 @@
+namespace backend.Application.Services
+{
+    using System.Collections.Generic;
+
+    public interface IEmailService
+    {
+        void SendEmail(string to, string subject, string body, string? attachmentBase64 = null, string? attachmentFilename = null, string? attachmentMimeType = null);
+        void SendEmailBatch(List<string> to, string subject, string body);
+    }
+}
