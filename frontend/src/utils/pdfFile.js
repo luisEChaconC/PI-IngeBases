59949ece6433b18bc93e8ff751cedcd50ef0a1d6
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-07-02 03:44:34 -0600

    feat(): add email sending methods to PdfFile class

--- frontend/src/utils/pdfFile.js
+++ frontend/src/utils/pdfFile.js
@@ -1,3 +1,7 @@
+import EmailService from '@/services/EmailService';
+
+const BODY = 'Se adjunta el archivo PDF';
+
 export class PdfFile {
     constructor(filename, wrapee) {
         this.filename = filename;
@@ -8,7 +12,27 @@ export class PdfFile {
         this.wrapee.save(this.filename);
     }
 
+    sendToEmail(subject, to) {
+        EmailService.sendEmail({
+            to: to,
+            subject: subject,
+            body: BODY,
+            attachmentBase64: this.getBase64(),
+            attachmentFilename: this.filename
+        });
+    }
+
+    sendToCurrentUserEmail(subject) {
+        const currentUserInformation = JSON.parse(localStorage.getItem('currentUserInformation'));
+        const email = currentUserInformation?.email;
+        if (email) {
+            this.sendToEmail(subject, email);
+        }
+    }
+
     getBase64() {
-        return this.wrapee.output('datauristring');
+        const dataUri = this.wrapee.output('datauristring');
+        // Remove the "data:application/pdf;base64," prefix to get only the base64 content
+        return dataUri.split(',')[1];
     }
 }
